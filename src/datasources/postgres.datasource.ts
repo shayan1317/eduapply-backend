95ad10c0a7a5f91265455e4df28434c058d0972c
shayan <shayanakhtar14@gmail.com>
2026-04-20 12:55:30 +0500

    refactor: simplify postgres datasource configuration to use environment variables and update ssl settings

--- src/datasources/postgres.datasource.ts
+++ src/datasources/postgres.datasource.ts
@@ -4,18 +4,11 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'postgres',
   connector: 'postgresql',
-  url:
-    process.env.DB_URI ?? 'postgres://postgres:123456@localhost:5432/EDUAPPLY',
-  host: process.env.DB_HOST ?? 'localhost',
-  password: process.env.DB_PASSWORD ?? '123456',
-  // doersDevServerConfigs
-  // url: 'postgres://postgres:NjUycbY67iY62z@dev-databases.chwxmrjifkep.us-east-2.rds.amazonaws.com:5432/EDUAPPLY',
-  // host: 'dev-databases.chwxmrjifkep.us-east-2.rds.amazonaws.com',
-  // password: 'NjUycbY67iY62z',
-  port: process.env.DB_PORT ?? 5432,
-  user: process.env.DB_USER ?? 'postgres',
-  database: process.env.DB_NAME ?? 'EDUAPPLY',
-  ssl: true,
+  url: process.env.DB_URI,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 20000,
 };
 
 @lifeCycleObserver('datasource')
